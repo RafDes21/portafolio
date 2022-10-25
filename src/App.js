4bc524a243adea52ac0077201f66a521631ea448
unknown <israeltineo84@gmail.com>
2022-10-25 11:47:21 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <h1>hola mundo</h1>
+      <h1>hola mundo</h1>
     </div>
   );
 }
